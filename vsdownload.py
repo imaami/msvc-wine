a770da3d1b7a1666a69b5e9a71c7c4dce6935b58
Juuso Alasuutari <juuso.alasuutari@gmail.com>
2025-02-25 16:46:37 +0200

    vsdownload: Specify MSVC 17.13 / SDK 10.0.22621 version combo
    Signed-off-by: Juuso Alasuutari <juuso.alasuutari@gmail.com>

--- vsdownload.py
+++ vsdownload.py
@@ -193,6 +193,8 @@ def setPackageSelection(args, packages):
         setPackageSelectionMSVC16(args, packages, args.msvc_version, "10.0.22621", "14.40.17.10", defaultPackages)
     elif args.msvc_version == "17.11":
         setPackageSelectionMSVC16(args, packages, args.msvc_version, "10.0.22621", "14.41.17.11", defaultPackages)
+    elif args.msvc_version == "17.13":
+        setPackageSelectionMSVC16(args, packages, args.msvc_version, "10.0.22621", "14.43.17.13", defaultPackages)
 
     elif args.msvc_version == "15.4":
         setPackageSelectionMSVC15(args, packages, args.msvc_version, "10.0.16299", "14.11", defaultPackages)
